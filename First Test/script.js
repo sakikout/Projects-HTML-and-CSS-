768ce2eb902fe75600ffd50ced4de6dea9a1c8b9
Bela D <86171066+sakikout@users.noreply.github.com>
2022-07-25 14:02:16 -0300

    Add files via upload

---
+++ First Test/script.js
@@ -0,0 +1,30 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+function spoilerisclicked(){
+  console.log("lol")
+}
+
+$('.slideshow-container').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4
+  });
+  
